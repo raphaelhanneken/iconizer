30ad18719bddf1c67f989f5b764054c28eb964e6
Raphael Hanneken <raphaelhanneken@me.com>
2015-06-06 18:15:31 +0200

    Initial commit

---
+++ Iconizer/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Iconizer
+//
+//  Created by Raphael Hanneken on 06.06.15.
+//  Copyright (c) 2015 Raphael Hanneken. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+}
+
