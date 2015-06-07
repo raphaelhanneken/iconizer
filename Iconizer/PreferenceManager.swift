a968f61061913fb962ef5e20ba20d9be0f0d4c41
Raphael Hanneken <raphaelhanneken@me.com>
2015-06-07 16:08:06 +0200

    Implemented PreferenceManager

--- Iconizer/PreferenceManager.swift
+++ Iconizer/PreferenceManager.swift
@@ -25,8 +25,74 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+
 import Cocoa
 
+// Keys to access the userDefaults
+private let generateForAppleWatchKey = "generateForAppleWatch"      // Generate icons for Apple Watch?
+private let generateForIPhoneKey     = "generateForIPhone"          // Generate icons for iPhone?
+private let generateForIPadKey       = "generateForIPad"            // Generate icons for iPad?
+private let generateForMacKey        = "generateForMac"             // Generate icons for Mac OS?
+private let generateForCarKey        = "generateForCar"             // Generate icons for CarPlay?
+private let combinedAssetKey         = "combinedAsset"              // Export selected Platforms into one asset catalog
+private let selectedExportTypeKey    = "selectedExportType"         // Selected export type
+
+
+/// Manage the user default settings.
 class PreferenceManager {
     
+     /// Holds the standart user defaults.
+    private let userDefaults = NSUserDefaults.standardUserDefaults()
+    
+    var generateForAppleWatch: Int {
+        get            { return userDefaults.integerForKey(generateForAppleWatchKey) }
+        set (newValue) { userDefaults.setInteger(newValue, forKey: generateForAppleWatchKey) }
+    }
+    
+    var generateForIPhone: Int {
+        get            { return userDefaults.integerForKey(generateForIPhoneKey) }
+        set (newValue) { userDefaults.setInteger(newValue, forKey: generateForIPhoneKey) }
+    }
+    
+    var generateForIPad: Int {
+        get            { return userDefaults.integerForKey(generateForIPadKey) }
+        set (newValue) { userDefaults.setInteger(newValue, forKey: generateForIPadKey) }
+    }
+    
+    var generateForMac: Int {
+        get            { return userDefaults.integerForKey(generateForMacKey) }
+        set (newValue) { userDefaults.setInteger(newValue, forKey: generateForMacKey) }
+    }
+    
+    var generateForCar: Int {
+        get            { return userDefaults.integerForKey(generateForCarKey) }
+        set (newValue) { userDefaults.setInteger(newValue, forKey: generateForCarKey) }
+    }
+    
+    var combinedAsset: Int {
+        get            { return userDefaults.integerForKey(combinedAssetKey) }
+        set (newValue) { userDefaults.setInteger(newValue, forKey: combinedAssetKey) }
+    }
+    
+    var selectedExportType: Int {
+        get            { return userDefaults.integerForKey(selectedExportTypeKey) }
+        set (newValue) { userDefaults.setInteger(newValue, forKey: selectedExportTypeKey) }
+    }
+    
+
+    init() {
+        registerDefaultPreferences()
+    }
+    
+    ///  Register the default preferences.
+    func registerDefaultPreferences() {
+        let defaults = [ generateForMacKey: NSOnState,
+                         generateForIPhoneKey: NSOnState,
+                         generateForIPadKey: NSOnState,
+                         generateForAppleWatchKey: NSOnState,
+                         generateForCarKey: NSOnState,
+                         combinedAssetKey: NSOffState ]
+        
+        self.userDefaults.registerDefaults(defaults)
+    }
 }
