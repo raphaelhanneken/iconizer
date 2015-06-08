4444e950ec5df1a5cadc7a8da7da7c0bda80861e
Raphael Hanneken <raphaelhanneken@me.com>
2015-06-08 18:03:10 +0200

    Implemented generateImagesFromImage(_:, withResolutions:)

--- Iconizer/ImageSet.swift
+++ Iconizer/ImageSet.swift
@@ -27,6 +27,50 @@
 
 import Cocoa
 
+///  ImageSet model. Generates necessary images and saves itself to the HD.
 class ImageSet: NSObject {
     
+     /// Holds the recalculated images.
+    var images: [String : NSImage] = [:]
+    
+    ///  Creates images with the given resolutions from the given image.
+    ///
+    ///  :param: image       Base image.
+    ///  :param: resolutions Resolutions to resize the given image to.
+    ///
+    ///  :returns: Returns true on success; False on failure.
+    func generateImagesFromImage(image: NSImage, withResolutions resolutions: [String]) -> Bool {
+        // Loop through the resolutions. The old fashioned way...
+        for var i = 0; i < resolutions.count; i++ {
+            // ...since the highest resolution is always the first,
+            // we can just save the original image as the highest selected resolution.
+            if i == 0 {
+                images[resolutions[i]] = image
+                continue;
+            }
+            
+            // Calculate the resolutions for 2x and 1x
+            if resolutions[i] == "@2x" {
+                // Calculate the 2x image from the 3x image
+                if let image = images["@3x"] {
+                    images["@2x"] = image.copyWithSize(NSSize(width: image.width / 2, height: image.height / 2))
+                }
+            }
+            else if resolutions[i] == "@1x" {
+                // Calculate the 1x from the 2x image.
+                if let image = images["@2x"] {
+                    images["@1x"] = image.copyWithSize(NSSize(width: image.width / 2, height: image.height / 2))
+                } else {
+                    // In case we don't have a 2x image.
+                    if let image = images["@3x"] {
+                        // Calculate the 1x from the 3x image.
+                        images["@1x"] = image.copyWithSize(NSSize(width: image.width / 3, height: image.height / 3))
+                    }
+                }
+            }
+        }
+        
+        
+        return true
+    }
 }
