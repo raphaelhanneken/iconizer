8e59d7bdad172fb71f750c9486a18eaebe13ccc9
Raphael Hanneken <raphaelhanneken@me.com>
2015-07-14 23:31:09 +0200

    Changed the export method to the new ContentsJSON

--- Iconizer/AppIcon.swift
+++ Iconizer/AppIcon.swift
@@ -33,29 +33,8 @@ let dirName = "Iconizer Assets"
 
 class AppIcon: NSObject {
     
-     /// Holds the resized images.
+    /// Holds the resized images.
     var images: [String : Array<[String : NSImage?]>] = [:]
-    
-     /// Holds the required image sizes for each platform.
-    var sizes: [String : [String : Int]] = [:]
-    
-    
-    override init() {
-        // required icon sizes for OS X
-        self.sizes["Mac"] = ["appicon-16@1x": 16, "appicon-16@2x": 32, "appicon-32@1x": 32, "appicon-32@2x": 64, "appicon-128@1x": 128, "appicon-128@2x": 256, "appicon-256@1x": 256, "appicon-256@2x": 512, "appicon-512@1x": 512, "appicon-512@2x": 1024]
-        
-        // required icon sizes for iPhone
-        self.sizes["iPhone"] = ["settings-@1x": 29, "settings-@2x": 58, "settings-@3x": 87, "spotlight-@2x": 80, "spotlight-@3x": 120, "appicon-@2x": 120, "appicon-@3x": 180, "oldAppicon-@1x": 57, "oldAppicon-@2x": 114]
-        
-        // required icon sizes for Apple Watch
-        self.sizes["Watch"] = ["notificationCenter-38mm@2x": 48, "notificationCenter-42mm@2x": 55, "companionSettings-@2x": 58, "companionSettings-@3x": 87, "appLauncher-38mm@2x": 80, "longLook-42mm@2x": 88, "quickLook-38mm@2x": 172, "quickLook-42mm@2x": 196]
-        
-        // required icon sizes for iPad
-        self.sizes["iPad"] = ["settings-@1x": 29, "settings-@2x": 58, "spotlight-@1x": 40, "spotlight-@2x": 80, "oldSpotlight-@1x": 50, "oldSpotlight-@2x": 100, "appicon-@1x": 76, "appicon-@2x": 152, "oldAppicon-@1x": 72, "oldAppicon-@2x": 144]
-        
-        // required icon sizes for CarPlay
-        self.sizes["Car"] = ["carplay-@1x": 120]
-    }
     
     ///  Generate all necessary images for each selected platform.
     ///
@@ -67,14 +46,12 @@ class AppIcon: NSObject {
             // Temporary array to hold the generated images.
             var images: [[String : NSImage?]] = []
             
-            // Get the image sizes for the current platform.
-            if let imageSizesForCurrentPlatform = self.sizes[platform] {
-                // Loop through the image sizes.
-                for (name, size) in imageSizesForCurrentPlatform {
-                    // Resize the given image.
-                    let resizedImage = image.copyWithSize(NSSize(width: size, height: size))
-                    // Append the resized image to the temporary image array.
-                    images.append([name : resizedImage])
+            // Create a new JSON object for the current platform.
+            let jsonData = ContentsJSON(forType: AssetType.AppIcon, andPlatforms: [platform])
+            
+            for imageData in jsonData.images {
+                if let size = imageData["expected-size"]?.toInt(), let filename = imageData["filename"] {
+                    images.append([filename : image.copyWithSize(NSSize(width: size, height: size))])
                 }
             }
             
@@ -89,56 +66,50 @@ class AppIcon: NSObject {
     ///  :param: combined Save as combined catalog?
     func saveAssetCatalogToURL(url: NSURL, asCombinedAsset combined: Bool) {
         // Manage the Contents.json
-        var jsonFile: JSONFile = JSONFile()
+        var jsonFile: ContentsJSON
         
-        // Loop through the platforms, we generated images for.
+        // Define where to save the asset catalog.
+        var setURL = url.URLByAppendingPathComponent("/\(dirName)/AppIcon/Images.xcassets/AppIcon.appiconset", isDirectory: true)
+        
+        // Loop through the selected platforms.
         for (platform, images) in self.images {
-            // Unless we generate a combined asset, create a new jsonFile for each platform.
+            // Override the setURL in case we don't generate a combined asset.
             if !combined {
-                // Manages building and saving of the contents.json
-                jsonFile = JSONFile()
-            }
-            // Holds the complete file url.
-            let iconsetURL: NSURL
-            
-            // Check wether we'll generate a combined assert or not.
-            if combined {
-                // For a combined asset catalog save the .xcasset directory into "Iconizer Assets"...
-                iconsetURL = url.URLByAppendingPathComponent("/\(dirName)/Images.xcassets/AppIcon.appiconset", isDirectory: true)
-            } else {
-                // ...otherwise create a folder for each platform.
-                iconsetURL = url.URLByAppendingPathComponent("/\(dirName)/\(platform)/Images.xcassets/AppIcon.appiconset", isDirectory: true)
+                setURL = url.URLByAppendingPathComponent("/\(dirName)/\(platform)/Images.xcassets/AppIcon.appiconset", isDirectory: true)
             }
             
-            // Create the required folder structure.
-            NSFileManager.defaultManager().createDirectoryAtURL(iconsetURL, withIntermediateDirectories: true, attributes: nil, error: nil)
+            // Create the necessary folders.
+            NSFileManager.defaultManager().createDirectoryAtURL(setURL, withIntermediateDirectories: true, attributes: nil, error: nil)
             
             // Loop through the images of the current platform.
-            for images in images {
-                // Unwrap the image name and image file.
-                for (name, image) in images {
-                    // Set the filename
-                    let filename = "\(platform.lowercaseString)_\(name).png"
-                    // Append the filename to the url
-                    let fileURL  = iconsetURL.URLByAppendingPathComponent(filename, isDirectory: false)
+            for image in images {
+                // Get each image + filename.
+                for (filename, image) in image {
+                    // Append the filename to the appiconset url.
+                    let fileURL = setURL.URLByAppendingPathComponent(filename, isDirectory: false)
                     
                     // Unwrap the image
-                    if let icon = image {
-                        // Get the PNG representation of the current image...
-                        let pngRep = icon.PNGRepresentation()!
-                        
-                        // ...and save it to the given url.
-                        if pngRep.writeToURL(fileURL, atomically: true) {
-                            // Save the image information to the JSON object.
-                            jsonFile.buildAppIconDictForImageNamed(filename, forPlatform: platform, sized: icon.size)
-                        } else {
-                            println("ERR: \(filename)")
-                        }
+                    if let icon = image?.PNGRepresentation() {
+                        icon.writeToURL(fileURL, atomically: true)
                     }
                 }
             }
-            // Tell the JSON object to write itself to the given file url.
-            jsonFile.writeJSONFileToURL(iconsetURL)
+            
+            // Handle the Contents.json for each platform.
+            if !combined {
+                // Get the Contents.json for the current platform...
+                jsonFile = ContentsJSON(forType: AssetType.AppIcon, andPlatforms: [platform])
+                // ...and save it to the given file url.
+                jsonFile.saveToURL(setURL)
+            }
+        }
+        
+        // Handle the Contents.json for all platforms at once.
+        if combined {
+            // Get the Contents.json for all selected platforms...
+            jsonFile = ContentsJSON(forType: AssetType.AppIcon, andPlatforms: self.images.keys.array)
+            // ...and save it to the given file url.
+            jsonFile.saveToURL(setURL)
         }
     }
 }
